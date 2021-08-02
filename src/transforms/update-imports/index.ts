0ed45ee8f1ed847682457d5d8858c714c107bb3a
Tyler Butler <tylerbu@microsoft.com>
2021-08-02 16:35:51 -0700

    comments

--- src/transforms/update-imports/index.ts
+++ src/transforms/update-imports/index.ts
@@ -6,25 +6,19 @@ import {
   uberPackageToPackages,
 } from '../data';
 
-// const { getParser } = require('codemod-cli').jscodeshift;
-// import { getOptions } from 'codemod-cli';
-
 const transform: Transform = (fileInfo, api, options) => {
   const js = api.jscodeshift;
+
+  /** If true, convert TO the uber-package; otherwise convert FROM the uber-package. */
   const toUber: boolean = options.uber || false;
-
-  // console.log(JSON.stringify(options));
-
   const sourcePackages = [...(toUber ? packagesToUberPackage : uberPackageToPackages).keys()];
   const targetPackages = toUber ? specifiersToUberPackage : specifiersToPackages;
-
-  // console.log(sourcePackages);
-  // console.log(targetPackages);
 
   const makeSpecifier = (imported: string, local: string) => {
     return js.importSpecifier(js.identifier(imported), js.identifier(local));
   };
 
+  /** Returns the first path in a collection. */
   const getFirstPath = (root: Collection) => {
     const c = root.find(js.Program);
     if (c.length > 0) {
@@ -32,10 +26,16 @@ const transform: Transform = (fileInfo, api, options) => {
     }
   };
 
+  /** Returns the first node in a collection. */
   const getFirstNode = (root: Collection) => {
-    return getFirstPath(root).node;
+    const p = getFirstPath(root);
+    if (p) {
+      return p.node;
+    }
   };
 
+  /** Filters a collection to only ImportDeclarations from one of the source packages (either the uber-package or the
+   * individual packages). */
   const filterSourceImports = (root: Collection): Collection<ImportDeclaration> => {
     return root.find(js.ImportDeclaration).filter((path) => {
       // Include only imports from source packages
@@ -68,8 +68,8 @@ const transform: Transform = (fileInfo, api, options) => {
         } else {
           finalImportMap.set(newPkg!, [makeSpecifier(importedName, localName!)]);
         }
-        // api.report(`WRITING: import {${importedName}} from "${newPkg}";`);
       } else {
+        // This means we found an import that isn't in our mapping.
         api.report(`MISSING: ${importedName}`);
       }
     });
@@ -78,13 +78,26 @@ const transform: Transform = (fileInfo, api, options) => {
     rewrittenImports.push(js.importDeclaration(specifiers, js.literal(pkg)));
   }
 
-  if (rewrittenImports.length > 0) {
-    rewrittenImports[0].comments = comments;
-  }
-
+  // Remove the old imports and convert to a string to "save" it
   const outSrc = filterSourceImports(initialAST).remove().toSource();
-  const output = js(outSrc).find(js.Statement).at(0).insertBefore(rewrittenImports);
-  return output.toSource();
+
+  // Check for imports remaining in the source after we removed the ones we care about.
+  const importsRemaining = js(outSrc).find(js.ImportDeclaration);
+
+  if (importsRemaining.length > 0) {
+    // There are still imports, so attach the comment to the first one.
+    importsRemaining.at(0).forEach((path) => {
+      path.node.comments = comments;
+    });
+    const output = importsRemaining.at(-1).insertAfter(rewrittenImports);
+    return output.toSource();
+  } else {
+    if (rewrittenImports.length > 0) {
+      rewrittenImports[0].comments = comments;
+    }
+    const output = js(outSrc).find(js.Statement).at(0).insertBefore(rewrittenImports);
+    return output.toSource();
+  }
 };
 
 module.exports = transform;
